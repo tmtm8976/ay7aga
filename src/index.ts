bf973b4738ed8351a7957a8c679b572825abf73a
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-06 20:53:36 +0300

    created index route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => Bun.file('./src/views/index.html'))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
