ddebef8fc906c27f8ce30993ff0d2f4f75531f42
tmtm8976 <fatemayasser8976@gmail.com>
2023-10-07 04:17:25 +0300

    "GET posts"

--- src/routes/handlers.ts
+++ src/routes/handlers.ts
@@ -8,6 +8,13 @@ const createPost = new Elysia().post('/', async ({ body }: any) => {
   await db.insert(snippets).values({ content: body.post });
 });
 
+const getPosts = new Elysia().get('/posts', async () => {
+  const res = await db.select().from(snippets);
+  console.log(res);
+  return res
+});
+
+
 const test = new Elysia().get('/test', () => 'test');
 
-export const appHandlers = new Elysia().use(createPost).use(test);
+export const appHandlers = new Elysia().use(createPost).use(test).use(getPosts);
